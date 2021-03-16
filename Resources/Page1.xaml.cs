12203045d28ad6c5ed0bea1985c9d6cf1cb8f4bd
vinezi <wewewe.ww52@gmail.com>
2021-03-16 20:47:36 +0300

    testing new arch

---
+++ Resources/Page1.xaml.cs
@@ -0,0 +1,86 @@
+﻿using library_accounting_system.Services;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace library_accounting_system.Resources
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        DataBaseHelper db = new DataBaseHelper();
+        public Page1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_SignIn(object sender, RoutedEventArgs e)
+        {
+            String loginUser = userLoginField.Text;
+            String passwordUser = userPasswordField.Password;
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login` = @uL AND `password` = @uP", db.getConnection());
+            command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = loginUser;
+            command.Parameters.Add("@uP", MySqlDbType.VarChar).Value = passwordUser;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+                MessageBox.Show("Yes");
+            else
+                MessageBox.Show("no");
+        }
+
+        private void Button_Register(object sender, RoutedEventArgs e)
+        {
+            String loginUser = NewUserLoginField.Text;
+            String passwordUser = NewUserPasswordField.Password;
+            String confirmUser = NewUserConfirmField.Password;
+
+            MySqlCommand command = new MySqlCommand("INSERT INTO `users` (`login`, `password`, `is_admin`) VALUES (@newUserLogin, @newUserPassword, '')", db.getConnection());
+
+            if (passwordUser == confirmUser)
+            {
+                command.Parameters.Add("@newUserLogin", MySqlDbType.VarChar).Value = loginUser;
+                command.Parameters.Add("@newUserPassword", MySqlDbType.VarChar).Value = passwordUser;
+            }
+            else
+            {
+                MessageBox.Show("error");
+
+            }
+
+            db.openConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+                MessageBox.Show("reg? OK");
+            else
+                MessageBox.Show("reg? not OK");
+
+
+            db.closeConnection();
+
+        }
+    }
+}
---
+++ Services/DataBaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_accounting_system.Services
+{
+    class DataBaseHelper
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=8889;username=root;password=root;database=library_accounting_system");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
